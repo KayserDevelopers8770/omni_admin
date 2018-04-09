857bd3702a85f9581175a722b815877a837a73b2
KayserDevelopers8770 <development@kayser.cl>
2018-04-09 18:23:56 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import NavV from './components/NavV'
+import NavH from './components/NavH'
+
+export default {
+  name: 'App',
+  components: {
+    NavV,
+    NavH
+  }
+}
+</script>
+
